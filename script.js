bbfe53ac1fd42bc966190df7f0aa881f474724c9
Mahamud Aden <mahamudadn@gmail.com>
2023-05-03 01:56:28 -0500

    defined addnumbers funtion aand called out

---
+++ script.js
@@ -0,0 +1,15 @@
+// lets define the yourFuntion.
+// returned the value 
+
+function addNumbers(num1, num2) {
+
+    return num1 + num2;
+}
+
+
+let result = addNumbers(2, 3);
+console.log('adding 2 & 3 will give us ' ,result); // should log out 5
+
+// adding 2 and 7 and 
+ result = addNumbers(2, 7);
+console.log('adding 2 & 7 will give us' , result);// should log out 9
